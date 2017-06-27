822f25bfcd55b93b475bbacb9a5e3c88b0701962
dmitriy-serdyuk <serdyuk.dmitriy@gmail.com>
2017-06-27 14:42:12 -0400

    Add attention

---
+++ attend_to_detect/model/attention.py
@@ -0,0 +1,35 @@
+import torch
+from torch import nn
+from torch.nn.functional import tanh, softmax
+
+
+class ContentAttention(nn.Module):
+    def __init__(self, dim):
+        super(ContentAttention, self).__init__()
+        self.linear_in = nn.Linear(dim, dim, bias=False)
+        self.linear_out = nn.Linear(dim * 2, dim, bias=False)
+        self.mask = None
+
+    def applyMask(self, mask):
+        self.mask = mask
+
+    def forward(self, input, context):
+        """
+        input: batch x dim
+        context: batch x sourceL x dim
+        """
+        targetT = self.linear_in(input).unsqueeze(2)  # batch x dim x 1
+
+        # Get attention
+        attn = torch.bmm(context, targetT).squeeze(2)  # batch x sourceL
+        if self.mask is not None:
+            attn.data.masked_fill_(self.mask, -float('inf'))
+        attn = softmax(attn)
+        attn3 = attn.view(attn.size(0), 1, attn.size(1))  # batch x 1 x sourceL
+
+        weightedContext = torch.bmm(attn3, context).squeeze(1)  # batch x dim
+        contextCombined = torch.cat((weightedContext, input), 1)
+
+        contextOutput = tanh(self.linear_out(contextCombined))
+
+        return contextOutput, attn

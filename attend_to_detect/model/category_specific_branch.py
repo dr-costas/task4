cef1ddff73a64b76d4cc7057d8574f5eeb6ba8dd
Konstantinos Drossos <konstantinos.drossos@tut.fi>
2017-06-28 18:55:15 -0400

    Added the bare class for the category specific branch.

---
+++ attend_to_detect/model/category_specific_branch.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# imports
+import torch
+
+
+class CategoryBranch(torch.nn.Module):
+
+    def __init__(self):
+        super(CategoryBranch, self).__init__()
+        # TODO: Add the branch layers: CNN(s) followed by RNNs.
+
+    def forward(self, *inputs):
+        # TODO: Implement the forward passing
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF

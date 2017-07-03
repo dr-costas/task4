5b3ef2fc4c870e1f48a9dd069220c9f4f3840527
dmitriy-serdyuk <serdyuk.dmitriy@gmail.com>
2017-07-03 18:44:57 -0400

    Add encoder and initial state

--- attend_to_detect/model/category_specific_branch.py
+++ attend_to_detect/model/category_specific_branch.py
@@ -1,21 +1,44 @@
 import torch
 
+from torch import nn
+from torch.autograd import Variable
 from torch.nn import GRUCell, Linear
 from torch.nn.functional import cross_entropy
 
 from .attention import GaussianAttention
 
 
-class CategoryBranch(torch.nn.Module):
-    def __init__(self, decoder_dim, output_classes, monotonic_attention=False,
-                 bias=False):
+class Encoder(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(Encoder, self).__init__()
+        # TODO: proper encoder
+        self.convnet = Linear(input_dim, output_dim)
+
+    def forward(self, input_):
+        input_flat = input_.view(input_.size(0), -1)
+        output = self.convnet(input_flat)
+        output3d = output.view(output.size(0), input_.size(1), input_.size(2))
+        return output3d
+
+
+class CategoryBranch(nn.Module):
+    def __init__(self, input_dim, decoder_dim, output_classes,
+                 monotonic_attention=False, bias=False):
         super(CategoryBranch, self).__init__()
         # TODO: Add the branch layers: CNN(s) followed by RNNs.
+
+        self.decoder_dim = decoder_dim
 
         self.attention = GaussianAttention(
             dim=decoder_dim, monotonic=monotonic_attention, bias=bias)
         self.decoder_cell = GRUCell(decoder_dim, decoder_dim, bias=bias)
         self.output_linear = Linear(decoder_dim, output_classes)
+
+        self.encoder = Encoder(input_dim, decoder_dim)
+
+    @property
+    def is_cuda(self):
+        return next(self.parameters()).is_cuda
 
     def encode(self, input_):
         """Produces contexts
@@ -23,19 +46,24 @@ class CategoryBranch(torch.nn.Module):
         Runs CNN(s) followed by RNN(s)
 
         """
-        raise NotImplementedError()
+        return self.encoder(input_)
 
-    def get_initial_hidden(self, input_, context):
-        raise NotImplementedError()
+    def get_initial_state(self, input_, context):
+        # TODO: smarter initial state
+        state = Variable(torch.zeros((input_.size(0), self.decoder_dim)))
+        if self.is_cuda:
+            state = state.cuda()
+        return state
 
     def forward(self, input_, output):
         context = self.encode(input_)
 
-        hidden = self.get_initial_hidden(input_, context)
+        hidden = self.get_initial_state(input_, context)
         kappa = self.attention.get_inital_kappa(context)
-        out_hidden = []
 
+        out_hidden = []
         out_weights = []
+
         for i in range(output.size(1)):
             attended, weights, kappa = self.attention(hidden, context, kappa)
             hidden = self.decoder_cell(attended, hidden)

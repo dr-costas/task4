771c0e40f0170d03a0ce55947530baf093011b5f
dmitriy-serdyuk <serdyuk.dmitriy@gmail.com>
2017-07-03 18:20:53 -0400

    Refactor attention and create a mock category branch

--- attend_to_detect/model/category_specific_branch.py
+++ attend_to_detect/model/category_specific_branch.py
@@ -1,26 +1,40 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# imports
 import torch
+
+from torch.nn import GRUCell
+
+from .attention import GaussianAttention
 
 
 class CategoryBranch(torch.nn.Module):
-
-    def __init__(self):
+    def __init__(self, decoder_dim, bias=False):
         super(CategoryBranch, self).__init__()
         # TODO: Add the branch layers: CNN(s) followed by RNNs.
 
-    def forward(self, *inputs):
-        # TODO: Implement the forward passing
-        pass
+        self.attention = GaussianAttention(dim=decoder_dim, bias=bias)
+        self.decoder_cell = GRUCell(decoder_dim, decoder_dim, bias=bias)
 
+    def encode(self, input_):
+        """Produces contexts
 
-def main():
-    pass
+        Runs CNN(s) followed by RNN(s)
 
+        """
+        raise NotImplementedError()
 
-if __name__ == '__main__':
-    main()
+    def get_initial_hidden(self, input_, output):
+        raise NotImplementedError()
 
-# EOF
+    def forward(self, input_, output):
+        context = self.encode(input_)
+
+        hidden = self.get_initial_hidden(input_, output)
+        kappa = self.attention.get_inital_kappa(context)
+        out_hidden = []
+        for i in range(output.size(1)):
+            attended = self.attention(hidden, context, kappa)
+            hidden = self.decoder_cell(attended, hidden)
+            out_hidden.append(hidden)
+        return out_hidden
+
+    def cost(self, predicted, target):
+        raise NotImplementedError()

1cdb8c6f79423ec5cb5066e09900aa20b5c99fa2
dmitriy-serdyuk <serdyuk.dmitriy@gmail.com>
2017-07-03 18:35:21 -0400

    Add cost

--- attend_to_detect/model/category_specific_branch.py
+++ attend_to_detect/model/category_specific_branch.py
@@ -1,17 +1,21 @@
 import torch
 
-from torch.nn import GRUCell
+from torch.nn import GRUCell, Linear
+from torch.nn.functional import cross_entropy
 
 from .attention import GaussianAttention
 
 
 class CategoryBranch(torch.nn.Module):
-    def __init__(self, decoder_dim, bias=False):
+    def __init__(self, decoder_dim, output_classes, monotonic_attention=False,
+                 bias=False):
         super(CategoryBranch, self).__init__()
         # TODO: Add the branch layers: CNN(s) followed by RNNs.
 
-        self.attention = GaussianAttention(dim=decoder_dim, bias=bias)
+        self.attention = GaussianAttention(
+            dim=decoder_dim, monotonic=monotonic_attention, bias=bias)
         self.decoder_cell = GRUCell(decoder_dim, decoder_dim, bias=bias)
+        self.output_linear = Linear(decoder_dim, output_classes)
 
     def encode(self, input_):
         """Produces contexts
@@ -21,20 +25,26 @@ class CategoryBranch(torch.nn.Module):
         """
         raise NotImplementedError()
 
-    def get_initial_hidden(self, input_, output):
+    def get_initial_hidden(self, input_, context):
         raise NotImplementedError()
 
     def forward(self, input_, output):
         context = self.encode(input_)
 
-        hidden = self.get_initial_hidden(input_, output)
+        hidden = self.get_initial_hidden(input_, context)
         kappa = self.attention.get_inital_kappa(context)
         out_hidden = []
-        for i in range(output.size(1)):
-            attended = self.attention(hidden, context, kappa)
-            hidden = self.decoder_cell(attended, hidden)
-            out_hidden.append(hidden)
-        return out_hidden
 
-    def cost(self, predicted, target):
-        raise NotImplementedError()
+        out_weights = []
+        for i in range(output.size(1)):
+            attended, weights, kappa = self.attention(hidden, context, kappa)
+            hidden = self.decoder_cell(attended, hidden)
+
+            out_hidden.append(self.output_linear(hidden).unsqueeze(1))
+            out_weights.append(weights)
+        return torch.cat(out_hidden, dimension=1), weights
+
+    def cost(self, out_hidden, target):
+        out_hidden_flat = out_hidden.view(out_hidden.size(0), -1)
+        target_flat = target.view(target.size(0), -1)
+        return cross_entropy(out_hidden_flat, target_flat)

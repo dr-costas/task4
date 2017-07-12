82f09a64cbcc3b7af2d1f110ccef7a523e180670
Konstantinos Drossos <konstantinos.drossos@tut.fi>
2017-07-12 18:34:05 -0400

    Added init file to scripts dir.

---
+++ scripts/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# imports
+
+
+__author__ = 'Konstantinos Drossos -- TUT'
+__docformat__ = 'reStructuredText'
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF

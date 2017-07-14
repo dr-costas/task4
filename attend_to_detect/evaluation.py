03b54470de21f63e695ca2bfad77f24eb10ff2e1
Konstantinos Drossos <konstantinos.drossos@tut.fi>
2017-07-14 16:07:59 -0400

    Fixed the evaluation according to sed_eval

--- attend_to_detect/evaluation.py
+++ attend_to_detect/evaluation.py
@@ -5,7 +5,7 @@ from torch.nn.functional import cross_entropy, softmax, binary_cross_entropy, si
 from sed_eval.sound_event import SegmentBasedMetrics
 
 from attend_to_detect.dataset import (
-    vehicle_classes, alarm_classes, get_input, get_output)
+    vehicle_classes, alarm_classes, get_input, get_output_binary)
 
 
 MAX_VALIDATION_LEN = 5
@@ -71,78 +71,79 @@ def validate(valid_data, common_feature_extractor, branch_alarm, branch_vehicle,
         x = get_input(batch[0], scaler, volatile=True)
 
         # Get target values for alarm classes
-        y_alarm_1_hot, y_alarm_logits = get_output(batch[-2])
+        y_alarm_1_hot, y_alarm_logits = get_output_binary(batch[-2])
 
         # Get target values for vehicle classes
-        y_vehicle_1_hot, y_vehicle_logits = get_output(batch[-1])
+        y_vehicle_1_hot, y_vehicle_logits = get_output_binary(batch[-1])
 
         # Go through the common feature extractor
         common_features = common_feature_extractor(x)
 
         # Go through the alarm branch
         alarm_output, alarm_weights = branch_alarm(
-            common_features, MAX_VALIDATION_LEN)
+            common_features, len(alarm_classes))
 
         # Go through the vehicle branch
         vehicle_output, vehicle_weights = branch_vehicle(
-            common_features, MAX_VALIDATION_LEN)
+            common_features, len(vehicle_classes))
 
         # Calculate validation losses
         # Chopping of at the groundtruth length
 
-        alarm_output_aligned = alarm_output[:, :y_alarm_logits.size(1)].contiguous()
-        vehicle_output_aligned = vehicle_output[:, :y_vehicle_logits.size(1)].contiguous()
+        # alarm_output_aligned = alarm_output[:, :y_alarm_logits.size(1)].contiguous()
+        # vehicle_output_aligned = vehicle_output[:, :y_vehicle_logits.size(1)].contiguous()
 
-        loss_a += category_cost(alarm_output_aligned, y_alarm_logits).data[0]
-        loss_v += category_cost(vehicle_output_aligned, y_vehicle_logits).data[0]
+        loss_a += binary_category_cost(alarm_output, y_alarm_1_hot).data[0]
+        loss_v += binary_category_cost(vehicle_output, y_vehicle_1_hot).data[0]
 
-        accuracy_a += accuracy(alarm_output_aligned, y_alarm_logits)
-        accuracy_v += accuracy(vehicle_output_aligned, y_vehicle_logits)
+        # accuracy_a += accuracy(alarm_output_aligned, y_alarm_logits)
+        # accuracy_v += accuracy(vehicle_output_aligned, y_vehicle_logits)
 
         valid_batches += 1
 
         if torch.has_cudnn:
             alarm_output = alarm_output.cpu()
             vehicle_output = vehicle_output.cpu()
-            y_alarm_logits = y_alarm_logits.cpu()
-            y_vehicle_logits = y_vehicle_logits.cpu()
+            y_alarm_1_hot = y_alarm_1_hot.cpu()
+            y_vehicle_1_hot = y_vehicle_1_hot.cpu()
 
-        predictions_alarm.extend(softmax(alarm_output).data.numpy())
-        predictions_vehicle.extend(softmax(vehicle_output).data.numpy())
-        ground_truths_alarm.extend(y_alarm_logits.data.numpy())
-        ground_truths_vehicle.extend(y_vehicle_logits.data.numpy())
+        predictions_alarm.extend(sigmoid(alarm_output).data.numpy())
+        predictions_vehicle.extend(sigmoid(vehicle_output).data.numpy())
+        ground_truths_alarm.extend(y_alarm_1_hot.data.numpy())
+        ground_truths_vehicle.extend(y_vehicle_1_hot.data.numpy())
 
     print('Epoch {:4d} validation elapsed time {:10.5f}'
           '\n\tValid. loss alarm: {:10.6f} | vehicle: {:10.6f} '.format(
                 epoch, validation_start_time - timeit.timeit(),
                 loss_a/valid_batches, loss_v/valid_batches))
-    metrics_alarm = tagging_metrics_from_raw_output(
-        predictions_alarm, ground_truths_alarm, ['<EOS>'] + alarm_classes)
-    metrics_vehicle = tagging_metrics_from_raw_output(
-        predictions_vehicle, ground_truths_vehicle, ['<EOS>'] + vehicle_classes)
-    print(metrics_alarm)
-    print(metrics_vehicle)
+    metrics = tagging_metrics_from_raw_output(
+        predictions_alarm, predictions_vehicle,
+        ground_truths_alarm, ground_truths_vehicle,
+        alarm_classes, vehicle_classes)
+    print(metrics)
 
-    f_score_overall_alarm = metrics_alarm.overall_f_measure()
-    f_score_overall_vehicle = metrics_vehicle.overall_f_measure()
+    # f_score_overall_alarm = metrics_alarm.overall_f_measure()
+    # f_score_overall_vehicle = metrics_vehicle.overall_f_measure()
     logger.log({'iteration': total_iterations,
                 'epoch': epoch,
                 'records': {
                     'valid_alarm': dict(
                         loss=loss_a/valid_batches,
                         acc=accuracy_a/valid_batches,
-                        f_score=f_score_overall_alarm['f_measure'],
-                        precision=f_score_overall_alarm['precision'],
-                        recall=f_score_overall_alarm['recall']),
+                        # f_score=f_score_overall_alarm['f_measure'],
+                        # precision=f_score_overall_alarm['precision'],
+                        # recall=f_score_overall_alarm['recall']
+                    ),
                     'valid_vehicle': dict(
                         loss=loss_v/valid_batches,
                         acc=accuracy_v/valid_batches,
-                        f_score=f_score_overall_vehicle['f_measure'],
-                        precision=f_score_overall_vehicle['precision'],
-                        recall=f_score_overall_vehicle['recall'])}})
+                        # f_score=f_score_overall_vehicle['f_measure'],
+                        # precision=f_score_overall_vehicle['precision'],
+                        # recall=f_score_overall_vehicle['recall']
+                    )}})
 
 
-def tagging_metrics_from_list(y_pred_dict, y_true_dict, all_labels, file_prefix=''):
+def tagging_metrics_from_list(y_pred_dict, y_true_dict, all_labels):
     """
 
     :param y_pred_dict:
@@ -165,82 +166,72 @@ def tagging_metrics_from_list(y_pred_dict, y_true_dict, all_labels, file_prefix=
     return segment_based_metrics
 
 
-def tagging_metrics_from_raw_output(y_pred, y_true, all_labels, y_true_is_categorical=True):
+def tagging_metrics_from_raw_output(y_pred_a, y_pred_v, y_true_a, y_true_v, all_labels_a, all_labels_v):
     """
 
     If 1-hot-encoding, the value [1, 0, 0, ..., 0] is considered <EOS>. \
     If not 1-hot-encoded, the value 0 is considered <EOS>.
 
-    :param y_pred: List with predictions, of len = nb_files and each element \
+    :param y_pred_a: List with predictions, of len = nb_files and each element \
                    is a matrix of size [nb_events_detected x nb_labels]
-    :type y_pred: numpy.core.multiarray.ndarray
-    :param y_true: List with true values, of len = nb_files. If 1-hot-enconding \
+    :type y_pred_a: numpy.core.multiarray.ndarray
+    :param y_true_a: List with true values, of len = nb_files. If 1-hot-enconding \
                    each element is a matrix of size [nb_events x nb_labels] . \
                    Else, each element is an array of len = nb_events.
-    :type y_true: numpy.core.multiarray.ndarray
-    :param all_labels: A list with all the labels **including** the <EOS> at the 0th index
-    :type all_labels: list[str]
-    :param y_true_is_categorical: Indication if y_true is categorical
-    :type y_true_is_categorical: bool
+    :type y_true_a: numpy.core.multiarray.ndarray
+    :param all_labels_a: A list with all the labels **including** the <EOS> at the 0th index
+    :type all_labels_a: list[str]
+    :param y_pred_v: List with predictions, of len = nb_files and each element \
+                   is a matrix of size [nb_events_detected x nb_labels]
+    :type y_pred_v: numpy.core.multiarray.ndarray
+    :param y_true_v: List with true values, of len = nb_files. If 1-hot-enconding \
+                   each element is a matrix of size [nb_events x nb_labels] . \
+                   Else, each element is an array of len = nb_events.
+    :type y_true_v: numpy.core.multiarray.ndarray
+    :param all_labels_v: A list with all the labels **including** the <EOS> at the 0th index
+    :type all_labels_v: list[str]
     :return:
     :rtype:
     """
-    def get_data(the_data, the_labels, is_categorical):
+    def get_data(the_data, the_labels):
         all_files_list = []
+        rounded_data = np.round(the_data)
 
-        for i, file_data in enumerate(the_data):
+        for file_data in rounded_data:
             file_list = []
-            for event in file_data:
-                if is_categorical:
-                    index = event
-                else:
-                    index = np.argmax(event)
-
-                if index == 0:
-                    break
-
-                file_list.append(
-                    {
-                        'event_offset': 10.00,
-                        'event_onset': 00.00,
-                        'event_label': the_labels[int(index)]
-                    }
-                )
+            for i, class_data in enumerate(file_data):
+                if class_data == 1:
+                    file_list.append(
+                        {
+                            'event_offset': 10.00,
+                            'event_onset': 00.00,
+                            'event_label': the_labels[i]
+                        }
+                    )
             all_files_list.append(file_list)
 
         return all_files_list
 
-    data_pred = get_data(y_pred, all_labels, False)
-    data_true = get_data(y_true, all_labels, y_true_is_categorical)
+    data_pred_a = get_data(y_pred_a, all_labels_a)
+    data_true_a = get_data(y_true_a, all_labels_a)
 
-    return tagging_metrics_from_list(data_pred, data_true, all_labels[1:])
+    data_pred_v = get_data(y_pred_v, all_labels_v)
+    data_true_v = get_data(y_true_v, all_labels_v)
+
+    data_pred = []
+    for f_data_a, f_data_v in zip(data_pred_a, data_pred_v):
+        data_pred.append(f_data_a + f_data_v)
+
+    data_true = []
+    for f_data_a, f_data_v in zip(data_true_a, data_true_v):
+        data_true.append(f_data_a + f_data_v)
+
+    return tagging_metrics_from_list(data_pred, data_true, all_labels_a + all_labels_v)
 
 
 def main():
     # Test code
-    labels = ['eos', 'car', 'bus', 'truck', 'bike']
-    nb_files = 10
-
-    x = np.random.random((nb_files, 3, len(labels)))
-    y_1_hot = np.zeros((nb_files, 3, len(labels)))
-    for i in range(nb_files):
-        random_col = np.random.choice(len(labels), 3, replace=False)
-        for ii, v in enumerate(random_col):
-            y_1_hot[i, ii, v] = 1
-
-    y_categorical = np.zeros(y_1_hot.shape[:-1])
-
-    for i, e in enumerate(y_1_hot):
-
-        events_ids = []
-
-        for ee in e:
-            events_ids.append(np.argmax(ee).astype('int'))
-
-        y_categorical[i, :] = events_ids
-
-    x = tagging_metrics_from_raw_output(x, y_categorical, labels, y_true_is_categorical=True)
-    print(tagging_metrics_from_raw_output(x, y_1_hot, labels, y_true_is_categorical=False))
+    pass
 
 
 if __name__ == '__main__':

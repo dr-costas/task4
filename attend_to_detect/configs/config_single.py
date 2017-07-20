844dc7ee32610c86b14ba58f14e621578a31e513
Konstantinos Drossos <kostas.drosos@icloud.com>
2017-07-20 02:16:45 -0400

    Small changes.

--- attend_to_detect/configs/config_single.py
+++ attend_to_detect/configs/config_single.py
@@ -7,7 +7,7 @@ batch_size = 64
 epochs = 300
 dataset_full_path = '/data/lisatmp4/santosjf/task4/attend_to_detect/create_dataset/dcase_2017_task_4_test.hdf5'
 
-grad_clip_norm = 1.
+grad_clip_norm = 0.
 network_loss_weight = True
 
 # Optimizer parameters
@@ -27,7 +27,7 @@ l2_factor = 1e-4
 # network_pool_strides = [(1, 2), (2, 2), (2, 2), (2, 3)]
 # network_pool_paddings = [(0, 1), (1, 1), (0, 1), (0, 2)]
 
-network_channels_out = [128, 128, 128]  # , 128]
+network_channels_out = [64, 128, 256]  # , 128]
 network_cnn_kernel_sizes = [(3, 3), (3, 3), (3, 3)]  # , (3, 3)]
 network_cnn_strides = [(2, 2), (2, 2), (2, 2)]  # , (2, 2)]
 network_cnn_paddings = [(1, 1), (1, 1), (1, 1)]  # , (1, 1)]
@@ -37,14 +37,14 @@ network_pool_kernels = [(3, 3), (3, 3), (3, 3)]  # , (3, 3)]
 network_pool_strides = [(2, 2), (2, 2), (2, 2)]  # , (2, 2)]
 network_pool_paddings = [(1, 1), (1, 1), (1, 1)]  # , (1, 1)]
 
-network_rnn_input_size = 128
+network_rnn_input_size = 256
 network_rnn_output_dims = [128, 128]  #, 256]
 network_rnn_activations = [functional.tanh, functional.tanh]  #, functional.tanh]
 network_attention_bias = True
 network_init = xavier_normal
 
-network_dropout_cnn = 0.25
-network_dropout_rnn_input = 0.25
+network_dropout_cnn = 0.5
+network_dropout_rnn_input = 0.5
 network_dropout_rnn_recurrent = 0.0
 
 network_rnn_subsamplings = [1]

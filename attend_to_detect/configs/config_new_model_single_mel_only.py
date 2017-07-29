d4a0c9a31a46496feaab1249af8443e391eaded0
Konstantinos Drossos <kostas.drosos@icloud.com>
2017-07-29 18:08:46 -0400

    New config for new model.

--- attend_to_detect/configs/config_new_model_single_mel_only.py
+++ attend_to_detect/configs/config_new_model_single_mel_only.py
@@ -23,11 +23,11 @@ lr_factor = .1
 # Optimizer parameters
 optimizer = Adadelta
 # optimizer = Adam
-optimizer_lr = 1e-5
+optimizer_lr = 1e-6
 l1_factor = 0.
 l2_factor = 0.
 
-network_channels_out = [64, 64, 64, 64]
+network_channels_out = [128, 128, 128, 128]
 network_cnn_kernel_sizes = [(5, 5), (5, 5), (5, 5), (5, 5)]
 network_cnn_strides = [(1, 1), (2, 2), (2, 2), (2, 2)]
 network_cnn_paddings = [(0, 0), (1, 1), (1, 1), (1, 1)]
@@ -47,8 +47,8 @@ network_pool_paddings = [(0, 0), (1, 1), (1, 1), (1, 1)]
 # network_pool_strides = [(2, 2), (2, 2), (2, 2)]  # , (2, 2)]
 # network_pool_paddings = [(1, 1), (1, 1), (1, 1)]  # , (1, 1)]
 
-network_rnn_input_size = 64
-network_rnn_output_dims = [32, 16]  #, 256]
+network_rnn_input_size = 128
+network_rnn_output_dims = [128, 128]  #, 256]
 
 network_rnn_activations = [functional.tanh]
 
@@ -62,8 +62,8 @@ network_dropout_rnn_recurrent = 0.25
 
 network_rnn_subsamplings = [1]
 
-mlp_dims = [16, len(all_freqs_vehicles_first)]
-mlp_activations = [functional.tanh]
+mlp_dims = [128, 64, len(all_freqs_vehicles_first)]
+mlp_activations = [functional.relu, functional.relu, functional.tanh]
 mlp_dropouts = [0.]
 
 last_rnn_dim = len(all_freqs_vehicles_first)

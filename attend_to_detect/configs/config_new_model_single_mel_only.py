09df2993ad43d6050d84d0f789b30f5046e423ae
Konstantinos Drossos <kostas.drosos@icloud.com>
2017-07-29 19:35:19 -0400

    Changes

--- attend_to_detect/configs/config_new_model_single_mel_only.py
+++ attend_to_detect/configs/config_new_model_single_mel_only.py
@@ -11,7 +11,7 @@ all_freqs_alarms_first = class_freqs_alarm + class_freqs_vehicle
 all_freqs_vehicles_first = class_freqs_vehicle + class_freqs_alarm
 
 # General variables
-batch_size = 16
+batch_size = 32
 epochs = 300
 dataset_full_path = '/data/lisatmp4/santosjf/task4/attend_to_detect/create_dataset/dcase_2017_task_4_test.hdf5'
 

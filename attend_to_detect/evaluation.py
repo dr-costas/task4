05da7828709855e747e6feede1e78455526c5168
Konstantinos Drossos <kostas.drosos@icloud.com>
2017-07-29 17:51:46 -0400

    More testing changes...

--- attend_to_detect/evaluation.py
+++ attend_to_detect/evaluation.py
@@ -953,6 +953,10 @@ def tagging_metrics_categorical(y_pred, y_true, all_labels, print_out=False):
 
         for i_f, file_data in enumerate(the_data):
             file_list = []
+            from IPython import embed
+            print(' -- ')
+            embed()
+            exit()
             for i_d, i in enumerate(file_data):
                 if i > 0.5:
                     file_list.append(
@@ -972,10 +976,6 @@ def tagging_metrics_categorical(y_pred, y_true, all_labels, print_out=False):
 
         for i_f, file_data in enumerate(the_data):
             file_list = []
-            from IPython import embed
-            print(' -- ')
-            embed()
-            exit()
             for i in file_data:
                 if i > -1:
                     file_list.append(
